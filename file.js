3302bdd9befc2efd9fe37dba1e8aa6ce1478f7e6
3rd-Eden <info@3rd-Eden.com>
2014-02-04 17:21:07 +0100

    [minor] Actually use the parser class

--- file.js
+++ file.js
@@ -1,23 +1,24 @@
 'use strict';
 
-/**
- * Parse the markdown information from the package.
- *
- * @param {Object} data The package.json or npm package contents.
- * @param {Function} next Continuation.
- * @api public
- */
-exports.parse = function parse(data, next) {
-  return exports;
-};
+module.exports = require('./parser').extend({
+  /**
+   * Parse the markdown information from the package.
+   *
+   * @param {Object} data The package.json or npm package contents.
+   * @param {Function} next Continuation.
+   * @api public
+   */
+  parse: function parse(data, next) {
+  },
 
-/**
- * Is markdown based license detection an option for this package.
- *
- * @param {Object} data The package.json or npm package contents.
- * @returns {Boolean}
- * @api public
- */
-exports.supported = function supported(data) {
-
-};
+  /**
+   * Is markdown based license detection an option for this package.
+   *
+   * @param {Object} data The package.json or npm package contents.
+   * @returns {Boolean}
+   * @api public
+   */
+  supported: function supported(data) {
+    return false;
+  }
+});
